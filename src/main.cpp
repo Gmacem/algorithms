08cf966958c3e8cb7f873f48f42a957f270a39ce
Gmasem <gmasem@yandex.ru>
2020-11-10 13:57:59 +0500

    remove submodule

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+//#include "graph.h"
+#include "iostream"
+#include "SQLite.h"
+
+int main() {
+//    BaseGraph graph(10);
+//    for (int i = 0; i < graph.Size(); ++i) {
+//        std::cout << graph.GetAdj(i).size() << '\n';
+//    }
+}
